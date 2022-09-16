744ee0185695fb55c3523341d0423cf07de88f76
Caled4 <7810907062@untrm.edu.pe>
2022-09-16 14:45:22 -0500

    ejercicio007

---
+++ ejercicio007.py
@@ -0,0 +1,29 @@
+# Al enumerar los primeros seis números primos: 2, 3, 5, 7, 11 y 13, podemos ver que el sexto primo es 13.
+# ¿Cuál es el número primo 10 001?
+# primos=[]
+def primo(nro):
+    nro=nro
+    for i in range(2,nro):
+        if nro%i==0:
+            return False
+    return True
+#-------------------
+# primo(10)
+#-------------------
+def arrayPrimo(nro):
+    primos =[]
+    for g in range(1,nro+1):
+        temp=primo(g)
+        if temp==True:
+            primos.append(g)
+    return primos
+#----------------
+# arrayPrimo(13)
+#---------------
+def ordenPrimo(nro):
+    print(len(arrayPrimo(nro)))
+
+#--------------------
+ordenPrimo(10001) 
+print(arrayPrimo(10001))
+#--------------------
