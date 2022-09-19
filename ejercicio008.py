5c5fe2058ed02dc66581cbd0dd58cc93d797d370
Caled4 <7810907062@untrm.edu.pe>
2022-09-18 21:34:57 -0500

    ejercicio008 --> codigo limpio

--- ejercicio008.py
+++ ejercicio008.py
@@ -1,53 +1,27 @@
-#ENUNCIADO:
-#  Los cuatro dígitos adyacentes en el número de 1000 dígitos que tienen el mayor producto son 9 × 9 × 8 × 9 = 5832.
 
-
-
-#  Encuentra los trece dígitos adyacentes en el número de 1000 dígitos que tienen el mayor producto. ¿Cuál es el valor de este producto?
-#--------------calcular la suma de una lista
 def productoLista(q):
     q=q
     producto=1
     for m in range(int(len(q))):
         producto=producto*int(q[m])
     return(producto)
-
-# print (productoLista([8,6,1]))
-
 def main(digitos):
     digitos=digitos-1
     flag=digitos
-    # suma=0
     rpta=[]
     mayor=0
-    # producto=1
-    #----------definimios data donde buscaremos
     data="7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450"
     data=list(data)
-    #--------recorremos la data
     for idx in range(int(len(data))):
-        # print(idx)
-            
         digitos+=1
         temp=[]
-        #--------creamos las convinaciones en la data
         for id in range(idx,digitos): 
-            #------cargamos los digitos en una tabla temporal ||||| [id-flag] ----> convierte nuestra data de manera linial a cicular obetniendo todas las convinaciones              
             temp.append(data[id-flag])
-            # print(data[id-flag])
-        # print(temp)
-        #------cargamos el producto de los digitos 
-
         prod=productoLista(temp)
-        # print(prod)
-        #-----seleccionamos el valor mas alto del producto de los digitos
         if mayor<prod :
             mayor=prod
             rpta=temp
-    #-----------------devolvemos solucion
     k="".join(rpta)
     print(f"en numero es {k}")
     print(f"la suma de sus cifras es : {mayor}")
-#-------------------ejecutar la funcion main y en el parametro colocar la cantidad de cifras del numero              
-
-main(4)
+main(13)
